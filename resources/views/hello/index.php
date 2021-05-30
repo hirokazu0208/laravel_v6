6a7fe7f0c319bddbda93c1f04f1b1f3a50628b95
hirokazu0208 <82913180+hirokazu0208@users.noreply.github.com>
2021-05-30 18:38:38 +0900

    テスト

---
+++ resources/views/hello/index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+
+<title>Hello/Index</title>
+
+<style>
+    body {
+        font-size:16pt;
+        color: #999;
+    }
+    h1{
+        font-size:100pt;
+        text-align: right;
+        color: #eee;
+        margin: -40px 0px -50px 0px;
+    }
+</style>
+</head>
+<body>
+<h1>Index</h1>
+<p>This is a sample page with php-template.2</p>
+</body>
+</html>
