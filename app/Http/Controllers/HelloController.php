22544e920d42253546c445476b793daba99a7c1f
hirokazu0208 <82913180+hirokazu0208@users.noreply.github.com>
2021-05-31 07:39:59 +0900

    3-5_3-6値をテンプレートに渡す

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -11,7 +11,9 @@ class HelloController extends Controller
 {
     public function index(){
 
-    return view('hello.index');
+        $data = ['msg'=>'これはコントローラから渡されたメッセージです。'];
+        
+        return view('hello.index', $data);
 
     }
 
--- resources/views/hello/index.php
+++ resources/views/hello/index.php
@@ -18,6 +18,8 @@
 </head>
 <body>
 <h1>Index</h1>
-<p>This is a sample page with php-template.2</p>
+<p><?php echo $msg; ?></p>
+<p><?php echo date("Y年n月j日"); ?></p>
+<p>This is a sample page with php-template.3</p>
 </body>
 </html>
